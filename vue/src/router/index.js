62401ca2581c9bb9e7feafd23b93564bfbecd57a
Ayman <ayman.patel97@gmail.com>
2022-03-18 10:05:03 +0530

    feat(Vue): Dashboard and Surveys

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,20 +2,19 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import DefaultLayout from "../views/DefaultLayout.vue";
 import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
 import Register from "../views/Register.vue";
 import Login from "../views/Login.vue";
 
 const routes = [
-  //   {
   {
-    path: "/dashboard",
-    name: "dashboard",
-    // redirect: "/dashboard",
-    component: Dashboard,
-    // children: [
-    //   { path: "/dashboard", name: "Dashboard", component: Dashboard },
-    //   { path: "/surveys", name: "Surveys", component: Surveys },
-    // ],
+    path: "/",
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys },
+    ],
   },
   {
     path: "/login",
